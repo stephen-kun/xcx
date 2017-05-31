dd8da3a2365e873e9d15f9522922f0295a94528c
Stephen <youqiukun@163.com>
2017-05-31 08:06:49 +0800

    clock

--- pages/clock/create/create.js
+++ pages/clock/create/create.js
@@ -10,6 +10,8 @@ for (let i = 0; i < 60; i++) {
   minutes.push(i)
 }
 
+var app = getApp()
+
 Page({
   /**
    * 页面的初始数据
@@ -17,63 +19,89 @@ Page({
   data: {
     clock:{
       hours: hours,
-      hour: 6,
       minutes: minutes,
-      minute: 30,
       value: [6, 30],
-      clocks: [],
       items:[
         { name: '每天', value: 'everyday' },
         { name: '周末', value: 'weekend' },
         { name: '临时', value: 'temporary', checked: 'true' },
         { name: '工作日', value: 'workingday' },
       ],
-      affair:"闹钟"
-    }
+      affair:"闹钟",
+    },
+    appid:null,
+    isLogin:false,
+    repeat: null,    
+    hour:null,
+    minute:null
+  },
+
+  radioChange:function(e){
+    console.log(e.detail.value)
+    this.setData({
+      repeat:e.detail.value
+    })
   },
 
   setClock: function (e) {
-    console.log("curr: " + this.data.clock.value)
+    console.log("affire: " + e.detail.value.affair)
+
+    if(!this.data.isLogin){
+      wx.showToast({
+        title: '您未注册',
+      })
+      return
+    }
+
     var clock = {};
-    clock["hour"] = this.data.clock.hour
-    clock["minute"] = this.data.clock.minute
-    var clocks = this.data.clock.clocks
-    var size = this.data.clocks.length
-    if (size > 0) {
-      var cnt = 0
-      for (var i = 0; i < size; i++) {
-        if ((clocks[i].hour != clock.hour) || (clocks[i].minute != clock.minute))
-          continue
-        else {
-          cnt++
-          break
+    clock["hour"] = this.data.hour
+    clock["minute"] = this.data.minute
+    clock['affair'] = e.detail.value.affair
+    clock['repeat'] = this.data.repeat
+
+    wx.request({
+      url: 'https://wx.tonki.com.cn/clock',
+      method:'POST',
+      data:{
+        appid:this.data.appid,
+        action:'add',
+        clock:clock
+      },
+      success:function(res){
+        if(res.data.err_no == 200){
+          wx.navigateBack({
+            delta:1
+          })
         }
       }
-      if (cnt == 0)
-        clocks.push(clock)
-    }
-    else
-      clocks.push(clock)
-    this.setData({
-      clocks: clocks
     })
-    console.log(this.data.clocks)
   },
 
   bindChange: function (e) {
     var val = e.detail.value
     console.log(val)
     this.setData({
-      hour: this.data.hours[val[0]],
-      minute: this.data.minutes[val[1]]
+      hour: this.data.clock.hours[val[0]],
+      minute: this.data.clock.minutes[val[1]]
     })
+    console.log(this.data.hour)
   },
 
   /**
  * 生命周期函数--监听页面加载
  */
   onLoad: function (options) {
-
+    var that = this
+    app.getAppId(function(appid){
+      that.setData({
+        appid:appid
+      })
+    })
+    
+    var isLogin = app.isLogin()
+    that.setData({
+      isLogin:isLogin
+    })
   },
 
   /**
